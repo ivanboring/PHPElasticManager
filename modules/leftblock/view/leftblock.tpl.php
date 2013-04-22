b37840802bdc6aba48c2e538e7883791ec6256d4
Marcus Johansson <me@marcusmailbox.com>
2013-04-22 20:03:21 +0200

    Problem with fixed leftmenu

--- modules/leftblock/view/leftblock.tpl.php
+++ modules/leftblock/view/leftblock.tpl.php
@@ -1,6 +1,7 @@
 <div class="indexBox">
+	<div class="filler"></div>
 <?php foreach($vars['indexes'] as $html) { echo $html; } ?>
 </div>
 
 <?php echo l('index/create', '<span class="button">Create new index</span>'); ?>
-
+<div class="filler"></div>
