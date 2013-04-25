021aa951245ef657036c4ffbace12aadb36f508a
Marcus Johansson <me@marcusmailbox.com>
2013-04-25 22:41:52 +0200

    Added license

--- modules/about/view/about.tpl.php
+++ modules/about/view/about.tpl.php
@@ -1,8 +1,6 @@
+<h3>Version</h3>
+<p>0.10-beta</p>
+<h3>More info</h3>
+<p><a href="http://www.phpelasticmanager.org">http://www.phpelasticmanager.org</a></p>
 <h3>License</h3>
-In short - as long as you are not a hosting company who wants to sell PHPElasticManager as part of the hosting, then you are free to do most things with it.
-
-<h4>For open source projects:</h4>
-<p>You are free to use PHPElasticAdmin under the GPL License. Any modified version should come with the same double license as this one.</p>
-
-<h4>For commercial hosting companies:</h4>
-<p>Commercial hosting companies who wants to offer PHPElasticManager as a service or as a preinstalled package are required to pay a license fee to do so. Please enquire for pricing to license@marcusmailbox.com.</p>
+<p>You are free to use PHPElasticAdmin under the <a href="http://opensource.org/licenses/MIT">MIT License</a>.</p>
